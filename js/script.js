7e84688a7126f91d8a852f114e12d9ceeddf0f5b
SlaykSD <60920077+SlaykSD@users.noreply.github.com>
2021-10-28 15:55:43 +0300

    LerningJS

---
+++ js/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+let number =10/0;
+const leftBorderWidth =1/23;
+//number = 3;
+console.log(number);
+const obj =  {
+    a: number,
+
+};
+const persone = {
+    name: "",
+    age: 0,
+    isMaried: false
+};
+persone.name ="Кукуха";
+obj.a = 12424;
+console.log(persone.name);
+let arr = ['asdfasdgf', 'aSFD SJK DAD F', 213123, {}, []];
+// for(int i= 0; i<arr.length; i++)
+// {
+//     console.log(arr[i]);
+// }
+//alert("hello world");
+//const result =  confirm("Are you here?");
+//console.log(result);
+//alert(result);
+// const answer = +prompt("Вам есть 18?", "13");
+// console.log(answer +1200);
+// const answers = [];
+// answers[0]= prompt('как ваше имя?','');
+// answers[1]= +prompt('Сколько лет?','');
+// answers[2]= prompt('Курите?','');
+// console.log(typeof(answers));
+const category = 'toys';
+console.log(`https://vk.com/${category}/5`);
+const user = 'Вася';
+console.log(4 + '2');
